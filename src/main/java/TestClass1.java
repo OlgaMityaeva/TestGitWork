821ba489e4e7c34abe34a44609dda752b7682cbc
Olga <olga.medintseva13@gmail.com>
2022-05-20 11:58:01 +0300

    Show ggg
    Squap commit

--- src/main/java/TestClass1.java
+++ src/main/java/TestClass1.java
@@ -2,5 +2,7 @@ public class TestClass1 {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("ggg");
+        String f = "lll";
+        String k = "zzz";
     }
 }
